076bc0e69f0908e81246d64e06c10f5de005aabd
Nyamawi Nyae <nyamawinyae258@gmail.com>
2026-03-17 04:45:13 +0000

    File changes

--- src/components/menu/MenuCard.jsx
+++ src/components/menu/MenuCard.jsx
@@ -39,9 +39,9 @@ const MenuCard = memo(function MenuCard({ item, onAddToCart }) {
 
   return (
     <motion.div
-      initial={{ opacity: 0, y: 20 }}
+      initial={{ opacity: 0, y: 10 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
+      transition={{ duration: 0.3 }}
       className="group"
     >
       <Link to={createPageUrl(`FoodDetails?id=${item.id}`)}>
